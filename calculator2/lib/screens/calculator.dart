22067c582eb02a8028ebc93ad7fa734057913d00
Victor Lamarck <lamarckvictor@hotmail.com>
2021-03-12 12:48:00 -0300

    projeto calculadora

---
+++ calculator2/lib/screens/calculator.dart
@@ -0,0 +1,34 @@
+import 'package:calculator/components/display.dart';
+import 'package:calculator/components/keyboard.dart';
+import 'package:calculator/models/memory.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return MaterialApp(
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Display(memory.value),
+          SizedBox(),
+          Keyboard(_onPressed),
+        ],
+      ),
+    );
+  }
+}
